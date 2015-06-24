8d01ec4de72cb65e1d04579ab8c512c0c5508ca1
kelvinabrokwa <kelvinabrokwa@gmail.com>
2015-06-24 15:42:41 -0400

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,13 @@
+brew install git
+brew install node
+brew install iojs
+brew install ack
+brew install tmux
+
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install spotify
+brew cask install slack
+brew cask install notational-velocity
+brew cask install licecap
